2d63dd573d28c36cba47a9786528891ba6cb8d2f
dirta121 <spekt7035@yandex.ru>
2022-09-23 18:34:31 +0300

    Add So class for business model

--- Assets/Scripts/BusinessSO.cs
+++ Assets/Scripts/BusinessSO.cs
@@ -2,29 +2,42 @@ namespace Advant.SO
 {
     using System;
     using UnityEngine;
+    using Advant.Intefaces;
+    using System.Collections.Generic;
+
     [CreateAssetMenu(menuName = "Advant/Business", fileName = "New Business")]
-    public class BusinessSO : ScriptableObject
+    public class BusinessSO : ScriptableObject, IBusiness
     {
         #region Editor fields
         [Tooltip("Id of current business")]
-        [SerializeField] private uint _id;
+        [SerializeField] uint _id;
         [Tooltip("Name of current business")]
-        [SerializeField] private string _name;
+        [SerializeField] string _name;
         [Tooltip("Delay of business income")]
-        [Range(0,30)]
-        [SerializeField] private float _incomeDelay;
+        [Range(0, 30)]
+        [SerializeField] float _incomeDelay;
         [Tooltip("Base cost of business")]
-        [SerializeField] private uint _baseCost;
+        [SerializeField] uint _baseCost;
         [Tooltip("Base income of business")]
-        [SerializeField] private uint _baseIncome;
-        [Tooltip("Array of available updates")]
-        [SerializeField] private BusinessUpdate[] _updates;
+        [SerializeField] uint _baseIncome;
+        [Tooltip("Start lvl of current business")]
+        [SerializeField] uint _lvl;
+        float _incomeProgress;
+        [Tooltip("Available updates")]
+        [SerializeField] List<BusinessUpdate> _updates;
+
         #endregion
-        public uint Id { get { return _id; } }
-        public string Name { get { return _name; } }
-        public float IncomeDelay { get { return _incomeDelay; } }
-        public float BaseCost { get { return _baseCost; } }
-        public float BaseIncome { get { return _baseIncome; } }
-        public BusinessUpdate[] Updates { get { return _updates; } }
+        public uint Id { get { return _id; }  set { _id = value; } }
+        public string Name { get { return _name; }  set { _name = value; } }
+        public float IncomeDelay { get { return _incomeDelay; }  set { _incomeDelay = value; } }
+        public uint BaseCost { get { return _baseCost; }  set { _baseCost = value; } }
+        public uint BaseIncome { get { return _baseIncome; }  set { _baseIncome = value; } }
+        public uint Lvl { get { return _lvl; } set { _lvl = value; } }
+        public float IncomeProgress { get { return _incomeProgress; }  set { _incomeProgress = value; } }
+        public List<BusinessUpdate> Updates { get { return _updates; } set { _updates = value; } }
+        public void ApplySettings(IBusiness business) { }      
+        public void SetContent() { }
+        public void EnableBusiness() { }
+        public void DisableBusiness() { }
     }
 }
